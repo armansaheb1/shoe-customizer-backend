a7e0594e9560298190ec81c8f36229aa6f2ca770
armansaheb1 <armansaheb@gmail.com>
2023-03-17 22:04:31 +0330

    Update

---
+++ main/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+
+from main.models import Category, Item, ItemMaterials, Material, Part, Pattern
+
+# Register your models here.
+admin.site.register(Item)
+admin.site.register(Part)
+admin.site.register(Material)
+admin.site.register(ItemMaterials)
+admin.site.register(Category)
+admin.site.register(Pattern)
